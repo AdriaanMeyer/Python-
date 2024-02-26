ceae760f6fe95ec28784d3865554aa09f50449bd
AdriaanMeyer <150583381+AdriaanMeyer@users.noreply.github.com>
2024-02-26 15:38:26 +0200

    Update Box and whiskers.py

--- Box and whiskers.py
+++ Box and whiskers.py
@@ -7,10 +7,29 @@ csv_file_path = '/Users/adriaanmeyer/Documents/GitHub/Python-/Example 1.csv'
 df = pd.read_csv(csv_file_path, sep=';')
 
 # Set the relevant column
-y_column = '2 Week Anti S Results'
+y1_column = '2 Week Anti S Results'
 
 # Create a box and whiskers chart for 2 Week Anti S Results
-sns.boxplot(y=df[y_column])
+sns.boxplot(y=df[y1_column])
 plt.title('Box and Whiskers Chart - 2 Week Anti S Results')
 plt.ylabel('2 Week Anti S Results')
 plt.show()
+
+# Set the relevant column
+y2_column = '4 Week Anti S Results'
+
+# Create a box and whiskers chart for 4 Week Anti S Results
+sns.boxplot(y=df[y2_column])
+plt.title('Box and Whiskers Chart - 4 Week Anti S Results')
+plt.ylabel('4 Week Anti S Results')
+plt.show()
+
+# Set the relevant column
+y3_column = '8 Week Anti S Results'
+
+# Create a box and whiskers chart for 8 Week Anti S Results
+sns.boxplot(y=df[y3_column])
+plt.title('Box and Whiskers Chart - 8 Week Anti S Results')
+plt.ylabel('8 Week Anti S Results')
+plt.show()
+
