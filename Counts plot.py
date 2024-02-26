727d50dbc0d58235cb1b3d09650a7063c3c8b9cb
AdriaanMeyer <150583381+AdriaanMeyer@users.noreply.github.com>
2024-02-26 15:17:47 +0200

    Update Counts plot.py

--- Counts plot.py
+++ Counts plot.py
@@ -3,17 +3,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load data from CSV with semicolon as separator
-csv_file_path = '/Users/adriaanmeyer/Desktop/SQL/Example 1.csv'
+csv_file_path = '/Users/adriaanmeyer/Documents/GitHub/Python-/Example 1.csv'
 df = pd.read_csv(csv_file_path, sep=';')
 
 # Extract relevant columns from DataFrame
 x_column = 'Age'
 
 # Seaborn countplot
-sns.countplot(x=x_column, data=df)
+sns.histplot(x=x_column, data=df)
 
 # Matplotlib customization
-plt.title('Count of Observations for Each Value')
+plt.title('Counts per age', fontsize=22)
 plt.xlabel(x_column)
 plt.ylabel('Count')
 plt.show()
+
